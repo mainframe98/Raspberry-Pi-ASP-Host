320d38667b96f9b221bd743821f0dbcd6ce6abb9
Klaas Skelte van der Werf <k.s.werf@hotmail.com>
2016-05-31 14:45:12 +0200

    Initial commit

---
+++ AspHost/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Owin.Hosting;
+
+namespace AspHost
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseUrl = "http://localhost:5000";
+            using (WebApp.Start<Startup>(baseUrl))
+            {
+                Console.WriteLine("Press Enter to quit.");
+                Console.ReadKey();
+            }
+    }
+}
---
+++ AspHost/Startup.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(AspHost.Startup))]
+
+namespace AspHost
+{
+    public class Startup
+    {
+
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseWelcomePage();
+            app.UseWelcomePage(new Microsoft.Owin.Diagnostics.WelcomePageOptions()
+            {
+                Path = new PathString("/welcome")
+            });
+
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/plain";
+
+                string output = string.Format(
+                    "I'm running on {0} nFrom assembly {1}",
+                    Environment.OSVersion,
+                    System.Reflection.Assembly.GetEntryAssembly().FullName
+                    );
+
+                return context.Response.WriteAsync(output);
+
+            });
+        }
+    }
+}
